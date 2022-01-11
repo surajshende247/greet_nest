83f96e640b5f60f302379479d2642b197eaecc52
Suraj Shende <surajshende247@gmail.com>
2022-01-11 13:04:11 +0530

    fix: syntax error

--- lib/greet_nest.rb
+++ lib/greet_nest.rb
@@ -3,9 +3,9 @@ require "greet_nest/version"
 module GreetNest
   class Error < StandardError; end
   
-  def self.greet(name)
-    "Hey #{name}!! I am in GreetNest module #{GreetNest::VERSION}. And the current time is #{Time.now}."
-  end
-end
+    def self.greet(name)
+      "Hey #{name}!! I am in GreetNest module #{GreetNest::VERSION}. And the current time is #{Time.now}."
+    end
+    
   end
 end
