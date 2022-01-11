a353907bfec274da2953700a2916da57fb291bcf
Suraj Shende <surajshende247@gmail.com>
2022-01-11 14:00:30 +0530

    feature: time based different greeting

--- lib/greet_nest.rb
+++ lib/greet_nest.rb
@@ -4,6 +4,23 @@ module GreetNest
   class Error < StandardError; end
   
     def self.greet(name)
-      "Hey #{name}!! I am in GreetNest module #{GreetNest::VERSION}. And the current time is #{Time.now}."
+      hour = Time.now.hour
+      
+      greeting = ""
+
+      if hour > 16 
+        greeting = "Good Evening"
+      elsif hour > 12
+        greeting = "Good Afternoon"
+      elsif hour > 7
+        greeting = "Good Morning"
+      elsif hour > 4
+        greeting = "Early Bird"
+      else
+        greeting = "Working Late"
+      end
+
+      "#{greeting}, #{name} !"
+    
     end
 end
